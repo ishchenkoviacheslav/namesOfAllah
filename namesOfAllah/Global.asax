a23f36add60dabca591c268a58f45d4363b67e04
Ищенко Вячеслав <ishchenkoviacheslav@live.com>
2017-06-02 19:50:18 +0300

    Add project files.

---
+++ namesOfAllah/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="namesOfAllah.MvcApplication" Language="C#" %>
---
+++ namesOfAllah/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(namesOfAllah.Startup))]
+namespace namesOfAllah
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
